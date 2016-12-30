04b147ff6d2e157e6c8b02d7f00468ea44266e24
benjaminboruff <ben@boruff.me>
2016-12-30 13:39:14 -0500

    divided states into smaller files

---
+++ src/states/load.js
@@ -0,0 +1,25 @@
+import Phaser from 'phaser'
+import { centerGameObjects } from '../utils'
+
+class Load extends Phaser.State {
+  init () {}
+
+  preload () {
+    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg');
+    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar');
+    centerGameObjects([this.loaderBg, this.loaderBar]);
+
+    this.load.setPreloadSprite(this.loaderBar);
+    //
+    // load your assets
+    //
+    this.load.image('mushroom', 'assets/images/mushroom2.png');
+  }
+
+  create () {
+    this.state.start('Play');
+  }
+
+}
+
+export default Load;
